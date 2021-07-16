d1284b4ba3fefb0698bd24f75fe57cc8c41bbeb9
Luka Cabraja <luka@cabraja.eu>
2021-07-16 16:44:21 +0300

    extension methods for string max length

---
+++ AdventureWorksOBPRepo/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorksOBPRepo
+{
+    static class Extensions
+    {
+        public static string WithMaxLength(this string value, int maxLength)
+        {
+            return value?.Substring(0, Math.Min(value.Length, maxLength));
+        }
+    }
+}
